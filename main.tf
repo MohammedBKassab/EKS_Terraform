3fe91ffa35999e23aa758d91a94d5d9a49b0679b
MohammedBKassab <Mohammedb.kassab@gmail.com>
2025-02-20 14:37:53 +0200

    Success :)

--- main.tf
+++ main.tf
@@ -1,7 +1,24 @@
-module "EKS" {
-    source = "./EKS_module"
+# # to use s3 backend 
+# # s3 bucket is configured at command line
+# terraform {
+#   backend "s3" {}
+# }
+
+# setup terraform aws provider to create resources in your desired aws region
+provider "aws" {
+  region = var.region
+
 }
 
-module "VPC" {
-    source = "./VPC_module"
+# invoke cluster module which creates vpc, subnets and eks cluter
+module "cluster" {
+  source = "./cluster"
+
+  # vpc details
+  vpc_name = var.vpc_name
+  vpc_cidr = var.vpc_cidr
+
+  # eks details
+  eks_cluster_name = var.cluster_name
+  k8s_version      = var.k8s_version
 }
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,14 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc_module.vpc_id
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.cluster.cluster_certificate_authority_data
 }
 
-output "subnet_ids" {
-  description = "List of subnet IDs"
-  value       = module.vpc_module.subnet_ids
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = module.cluster.cluster_endpoint
 }
 
-output "eks_cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.eks_module.cluster_name
-}
-
-output "eks_cluster_endpoint" {
-  description = "The endpoint of the EKS cluster"
-  value       = module.eks_module.cluster_endpoint
-}
-
-output "eks_node_group_ips" {
-  description = "List of node group IPs"
-  value       = module.eks_module.node_group_ips
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.cluster.cluster_oidc_issuer_url
 }
